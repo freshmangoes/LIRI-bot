d1546b4e72096cae12624fee273bc964c5d140e4
Kyle C <kylecilia@gmail.com>
2019-12-09 18:12:51 -0800

    Working on Spotify search, will print out song and artist name [WIP]

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# LIRI-bot
+## LIRI-bot
+
+A lightweight language interpretation and recognition interface (LIRI!).
+
+Utilizes Node, Axios, Bands in Town Artists API, and Spotify API. 
+
+- You will need your own Spotify ID and secret ID to run this.
+- Recommended to put these in .env and import them so they are inaccessible.
--- liri.js
+++ liri.js
@@ -2,30 +2,51 @@
 require("dotenv").config();
 const keys = require("./keys");
 const Spotify = require("node-spotify-api");
+const inquirer = require("inquirer");
 const axios = require("axios");
 const moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
-
-var spotifyQueryURL = "https://api.spotify.com/v1/search?q=the%20ramones";
+// var userQuery = `The Beatles Don't Let Me Down`
 
 console.log(`Keys: 
   ${keys.spotify.id}
   ${keys.spotify.secret}`);
 
-axios({
-  method: "get",
-  url: spotifyQueryURL,
-  // responseType: "stream",
-  data: {
-    access_token: `${keys.spotify.id}:${keys.spotify.secret}`,
-    token_type: "Bearer",
-    scope: "user-read-private"
-  }
-})
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(error) {
-    console.log(error);
+inquirer
+	.prompt([
+		{
+			type: "input",
+			message: "Input a Spotify search!",
+			name: "userQuery"
+		},
+		{
+			type: "confirm",
+			message: "Continue with this search?",
+			name: "confirm",
+			default: true
+		}
+	])
+	.then(function(response) {
+		if (response.confirm) {
+      console.log(`Spotify search!`);
+			spotify.search(
+				{
+					type: `track`,
+					query: response.userQuery
+				},
+				function(e, data) {
+					if (e) {
+						return console.log(`Error: ${e}`);
+          }
+          const items = data.tracks.items;
+          console.log("Data:", data);
+          console.log("Name:", JSON.stringify(data.name));
+          for(var i = 0; i < items.length; i++) {
+            const artist = items[i].artists[0].name;
+            console.log(`Item: ${JSON.stringify(items[i].name)} - Artist: ${JSON.stringify(artist)}`);
+          }
+				}
+			);
+		}
   });
